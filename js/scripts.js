de1378853c06f374dc88b4aabd78cd4cc8b80d3a
Sean Downs <spdowns@protonmail.com>
2020-03-27 10:19:33 -0700

    comments code out to try and make one button work as planned.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,11 @@ $(document).ready(function() {
   $("form#language").submit(function(event) {
     event.preventDefault();
     var xp = $("input:radio[name=xp]:checked").val();
-    console.log(xp);
-    var uiorux = $("input:radio[name=uiorux]:checked").val();
-    var os = $("input:radio[name=os]:checked").val();
-    var drink = $("input:radio[name=drink]:checked").val();
-    var music = $("input:radio[name=music]:checked").val();
+    // var uiorux = $("input:radio[name=uiorux]:checked").val();
+    // var os = $("input:radio[name=os]:checked").val();
+    // var drink = $("input:radio[name=drink]:checked").val();
+    // var music = $("input:radio[name=music]:checked").val();
 
-
+    // if(xp)
     var typeOfLanguage;
 });
