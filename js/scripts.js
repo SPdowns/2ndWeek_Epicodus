c396141ff9f539af318234d81ffffeb9a5b7fa78
Sean Downs <spdowns@protonmail.com>
2020-03-27 09:45:41 -0700

    add vars

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  
+  $("form#language").submit(function(event) {
+    event.preventDefault();
+    var xp = ($("input#xp").val());
+    var uiorux = ($("input#uiorux").val());
+    var os = ($("input#os").val());
+    var drink = ($("input#drink").val());
+    var music = ($("input#music").val());
 }
