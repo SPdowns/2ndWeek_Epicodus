f138286527fa3277933db1f4366508deee839c2c
Sean Downs <spdowns@protonmail.com>
2020-03-27 10:04:38 -0700

    console.log not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   $("form#language").submit(function(event) {
     event.preventDefault();
-    var xp = ($("input#xp").val());
-    var uiorux = ($("input#uiorux").val());
-    var os = ($("input#os").val());
-    var drink = ($("input#drink").val());
-    var music = ($("input#music").val());
-    console.log(userInput)
-}
+    var xp = $("input:radio[name=xp]:checked").val();
+console.log(xp)
+
+    var uiorux = $("input:radio[name=uiorux]:checked").val();
+    var os = $("input:radio[name=os]:checked").val();
+    var drink = $("input:radio[name=drink]:checked").val();
+    var music = $("input:radio[name=music]:checked").val();
+
+
+    var typeOfLanguage;
+});
