44419dda7b8788911c1d2146229fa6829467eafb
Sean Downs <spdowns@protonmail.com>
2020-03-27 09:47:43 -0700

    tests javascript with console.log

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,5 @@ $(document).ready(function() {
     var os = ($("input#os").val());
     var drink = ($("input#drink").val());
     var music = ($("input#music").val());
+    console.log(userInput)
 }
