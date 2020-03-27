a950133e540502fec4ff619a312aceefb44701e6
Sean Downs <spdowns@protonmail.com>
2020-03-27 10:42:21 -0700

    scope issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   $("form#language").submit(function(event) {
+    event.preventDefault();
     var xp = $("input:radio[name=xp]:checked").val();
     console.log(xp)
-    // var uiorux = $("input:radio[name=uiorux]:checked").val();
+    var uiorux = $("input:radio[name=uiorux]:checked").val();
+    console.log(uiorux)
     // var os = $("input:radio[name=os]:checked").val();
     // var drink = $("input:radio[name=drink]:checked").val();
     // var music = $("input:radio[name=music]:checked").val();
 
-    // if(xp)
     var typeOfLanguage;
+
+    if(xp === "beginner")
+    $("#typeOfLanguage").text(answer)
   });
 });
