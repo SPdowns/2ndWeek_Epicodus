a40642d7ecacaf9f6e4883dc58f5dbe00a36ea93
Sean Downs <spdowns@protonmail.com>
2020-03-27 11:11:43 -0700

    adds div classes for 3 responses

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,19 @@ $(document).ready(function() {
   $("form#language").submit(function(event) {
     event.preventDefault();
     var xp = $("input:radio[name=xp]:checked").val();
-    console.log(xp)
     var uiorux = $("input:radio[name=uiorux]:checked").val();
-    console.log(uiorux)
-    // var os = $("input:radio[name=os]:checked").val();
-    // var drink = $("input:radio[name=drink]:checked").val();
-    // var music = $("input:radio[name=music]:checked").val();
+    var os = $("input:radio[name=os]:checked").val();
+    var drink = $("input:radio[name=drink]:checked").val();
+    var music = $("input:radio[name=music]:checked").val();
 
     var typeOfLanguage;
 
     if ( xp === "beginner") {
       answer = "Javascript"
-    } else if (xp === "some" || xp === "lots") {
-    answer = "Ruby"
+    } else if (xp === "some" && uiorux === "front") {
+      answer = "Ruby"
+    } else if (xp === "lots") {
+      answer = "C#Sharp"
     }
 
     $("#typeOfLanguage").text(answer)
